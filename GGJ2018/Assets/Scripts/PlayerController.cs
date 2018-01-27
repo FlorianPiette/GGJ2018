0a1c911cd6c3e73ff9f1558494aa10970929f727
FloFrancois <f.francois@student.isartdigital.com>
2018-01-27 02:34:54 +0100

    Player1 + playermovement

--- GGJ2018/Assets/Scripts/PlayerController.cs
+++ GGJ2018/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	
-	void Start () {
-		
+
+	#region Variables
+
+	[SerializeField]
+	bool _allowedMovement = true;
+	bool _canMove;
+
+	public float _speedMove = 5f;
+
+	#endregion
+
+	#region Unity_methods
+
+	void OnEnable () {
+
+		Init();
 	}
-	
+
+	void OnDisable() {
+		_canMove = false;
+	}
+
+	void Init() {
+		_canMove = _allowedMovement;
+	}
+
 	void Update () {
-		
+		if (_canMove) {
+			float _xMov = Input.GetAxisRaw("Horizontal");
+			float _zMov = Input.GetAxisRaw("Vertical");
+
+			Vector3 _velocity = new Vector3(_xMov, 0, _zMov);
+
+			transform.position = transform.position + (_velocity * _speedMove) * Time.deltaTime;
+			if(_velocity != Vector3.zero) {
+				transform.rotation = Quaternion.LookRotation(_velocity);
+			}
+		}
 	}
+
+	#endregion
 }
