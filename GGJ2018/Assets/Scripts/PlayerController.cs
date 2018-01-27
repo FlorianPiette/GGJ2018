4d40f40bb146dd5d228c1c6ffb921dfe9dbc4d65
FloFrancois <f.francois@student.isartdigital.com>
2018-01-27 10:29:44 +0100

    Player1 + course + stamina

--- GGJ2018/Assets/Scripts/PlayerController.cs
+++ GGJ2018/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,24 @@ public class PlayerController : MonoBehaviour {
 
 	#region Variables
 
+	Animator _animator;
+
+	public int _playerIndex;
+
 	[SerializeField]
 	bool _allowedMovement = true;
 	bool _canMove;
 
-	public float _speedMove = 5f;
+	float _speedMove;
+	float _sprintSpeed;
+	float _boostSpeed;
 
+	float _stamina;
 	#endregion
 
 	#region Unity_methods
 
-	void OnEnable () {
-
+	void Start () {
 		Init();
 	}
 
@@ -26,20 +32,75 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Init() {
+		_animator = GetComponent<Animator>();
 		_canMove = _allowedMovement;
+		_speedMove = GameManager.Instance._startMoveSpeed;
+		_stamina = GameManager.Instance._startStamina;
+	}
+
+	void FixedUpdate() {
+		if (!Input.GetButton("J" + _playerIndex + "Abutton")) {
+			if(_stamina < GameManager.Instance._maxStamina * 0.5f) {
+				_stamina += GameManager.Instance._regenStamina;
+			}
+		}
 	}
 
 	void Update () {
+
+		if(Input.GetButtonUp("J" + _playerIndex + "Abutton")) {
+			_sprintSpeed = 0;
+		}
+
+
+		if(_stamina < GameManager.Instance._minStamina) {
+			_stamina = GameManager.Instance._minStamina;
+		}
+		if(_stamina > GameManager.Instance._maxStamina) {
+			_stamina = GameManager.Instance._maxStamina;
+		}
+
+
+		// Boost Here !
+
 		if (_canMove) {
-			float _xMov = Input.GetAxisRaw("Horizontal");
-			float _zMov = Input.GetAxisRaw("Vertical");
+			float _xMov = Input.GetAxisRaw("J" + _playerIndex + "Horizontal");
+			float _zMov = Input.GetAxisRaw("J" + _playerIndex + "Vertical");
 
 			Vector3 _velocity = new Vector3(_xMov, 0, _zMov);
 
-			transform.position = transform.position + (_velocity * _speedMove) * Time.deltaTime;
+			if(Input.GetButton("J" + _playerIndex + "Abutton")) {
+				if (_stamina < GameManager.Instance._limitToUseStamina) {
+					_sprintSpeed = 0;
+				} else {
+					_stamina -= GameManager.Instance._costStamina;
+					_sprintSpeed = GameManager.Instance._sprintSpeed;
+				}
+			}
+
+			transform.position = transform.position + (_velocity * (_speedMove + _sprintSpeed + _boostSpeed)) * Time.deltaTime;
 			if(_velocity != Vector3.zero) {
 				transform.rotation = Quaternion.LookRotation(_velocity);
 			}
+
+			if(_sprintSpeed != 0) {
+				_animator.SetBool("Boost", false);
+				_animator.SetBool("Sprint", true);
+				_animator.SetBool("Run", false);
+			} else if(_boostSpeed != 0) {
+				_animator.SetBool("Boost", true);
+				_animator.SetBool("Sprint", false);
+				_animator.SetBool("Run", false);
+			} else if(_velocity == Vector3.zero) {
+				_animator.SetBool("Boost", false);
+				_animator.SetBool("Sprint", false);
+				_animator.SetBool("Run", false);
+			} else {
+				_animator.SetBool("Boost", false);
+				_animator.SetBool("Sprint", false);
+				_animator.SetBool("Run", true);
+			}
+
 		}
 	}
 
---
+++ GGJ2018/Assets/Scripts/Tags.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tags {
+
+
+	#region Tags
+
+	public const string _gameManager = "GameManager";
+	public const string _mainCamera = "MainCamera";
+	public const string _player = "Player";
+
+	#endregion
+
+	#region Layer
+
+//	public const int c_layout_plane = 9;
+
+	#endregion
+
+	#region PlayerPrefs
+
+	#endregion
+}
